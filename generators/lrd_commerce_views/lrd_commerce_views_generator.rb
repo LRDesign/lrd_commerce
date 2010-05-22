f70e0d9eeb9e3d2328e92e62188ad2b7ab857229
Judson <nyarly@gmail.com>
2010-05-22 02:49:23 -0700

    Views made generic

--- generators/lrd_commerce_views/lrd_commerce_views_generator.rb
+++ generators/lrd_commerce_views/lrd_commerce_views_generator.rb
@@ -6,6 +6,26 @@ class LrdCommerceViewsGenerator < Rails::Generator::Base
       def create_payments_new
         template 'payments/new.html.haml.erb', 'app/views/payments/new.html.haml'
       end
+
+      def create_payments_index
+        template 'payments/index.html.haml.erb', 'app/views/payments/index.html.haml'
+      end
+
+      def create_payments_success
+        template 'payments/success.html.haml.erb', 'app/views/payments/success.html.haml'
+      end
+
+      def create_carts_edit
+        template 'carts/edit.html.haml.erb', 'app/views/carts/edit.html.haml'
+      end
+
+      def create_carts_item_partial
+        template '/carts/_cart_item.html.haml.erb', 'app/views/carts/edit.html.haml'
+      end
+
+      def create_payment_receipts_partial
+        template 'payment_receipts/_receipt.html.haml.erb', 'app/views/payment_receipts/_receipt.html.haml'
+      end
     end
   end
 end
