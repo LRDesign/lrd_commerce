aeb9f1795597e4b23b0ab4c74191ca11737bb289
Oliver Newland <onewland@gmail.com>
2010-06-16 08:58:29 -0700

    Test 'add to cart' behavior

--- spec/controllers/cart_items_controller_spec.rb
+++ spec/controllers/cart_items_controller_spec.rb
@@ -4,7 +4,54 @@ describe CartItemsController do
   before(:each) do
     @cart = Factory(:cart)
     session[:current_cart] = @cart.id
-    @item = @cart.items.create(:product => Factory(:product))
+  end
+
+  ##################################################################
+  ## POST CREATE
+  ##################################################################
+  describe "POST 'create'" do
+    describe "with a valid product ID" do
+      before(:each) do
+        @product = Factory(:product)
+      end
+
+      it "should create the cart item" do
+        post 'create', :cart_item => { :product_id => @product.id }
+        assigns[:cart_item].should_not be_nil
+        assigns[:cart_item].product.id.should == @product.id
+      end
+
+      it "should redirect back" do
+        # set back URL
+        back = @request.env['HTTP_REFERER'] = 'http://foo'
+        post 'create', :cart_item => { :product_id => @product.id }
+        response.should redirect_to(back)
+      end
+
+      it "should set flash for item added to cart" do
+        post 'create', :cart_item => { :product_id => @product.id }
+        flash[:notice].should == "#{@product.name} added to cart."
+      end
+    end
+
+    describe "with a bad a product ID" do
+      it "should not create/assign the cart item" do
+        post 'create', :cart_item => { :product_id => -1 }
+        assigns[:cart_item].should be_nil
+      end
+
+      it "should redirect back" do
+        # set back URL
+        back = @request.env['HTTP_REFERER'] = 'http://foo'
+        post 'create', :cart_item => { :product_id => -1 }
+        response.should redirect_to(back)
+      end
+
+      it "should set flash for failure" do
+        post 'create', :cart_item => { :product_id => -1 }
+        flash[:error].should == "Error adding item to cart."
+      end
+    end
   end
 
   ##################################################################
